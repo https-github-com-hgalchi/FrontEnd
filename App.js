42cbaf5e5ed2848391ea200596f90dcc5559db7c
MinJoon_Choi <minjoon1998@gmail.com>
2023-04-16 18:07:15 +0900

    Display marker information

--- App.js
+++ App.js
@@ -1,109 +1,219 @@
-import React, { useState, useEffect } from 'react';
-import { Platform, Text, View, StyleSheet, Button } from 'react-native';
-import * as Location from 'expo-location';
-import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
-import axios from 'axios';
-
-
+import React, { useState, useEffect } from "react";
+import {
+	Platform,
+	Text,
+	View,
+	StyleSheet,
+	Button,
+	Dimensions,
+	TouchableOpacity,
+} from "react-native";
+import * as Location from "expo-location";
+import MapView, { Marker, Callout, PROVIDER_GOOGLE } from "react-native-maps";
+import axios from "axios";
+import markersData from "./markerData";
 
 export default function App() {
-  const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
+	const [location, setLocation] = useState(null);
+	const [errorMsg, setErrorMsg] = useState(null);
+	const [markers, setMarkers] = useState([]);
+	// const [selectedMarker, setSelectedMarker] = useState(null);
+	const [markerInfo, setMarkerInfo] = useState(null);
 
+	//위치 변경될 때 마다 렌더링 되도록 요청
+	useEffect(() => {
+		(async () => {
+			let { status } = await Location.requestForegroundPermissionsAsync();
+			if (status !== "granted") {
+				setErrorMsg("Permission to access location was denied");
+				return;
+			}
 
-  useEffect(() => {
+			Location.watchPositionAsync(
+				{
+					accuracy: Location.Accuracy.Balanced,
+					timeInterval: 100,
+					distanceInterval: 1,
+				},
+				(position) => {
+					setLocation(position.coords);
+				}
+			);
+		})();
+	}, []);
 
+	//////////데이터 보내기/////////////
+	// const sendLocationData = async (location) => {
+	//   try {
+	//     const response = await axios.post("http://localhost:8080/map/location", {
+	//       latitude: location.latitude,
+	//       longitude: location.longitude,
+	//     });
 
-    (async () => {
+	//     console.log(response.data);
+	//   } catch (error) {
+	//     console.log(error);
+	//   }
+	// };
 
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
-        return;
-      }
+	// useEffect(() => {
+	//   if (location) {
+	//     sendLocationData(location);
+	//   }
+	// }, [location]);
 
-      // let location = await Location.getCurrentPositionAsync({accuracy:5});
-      // setLocation(location.coords);
-      Location.watchPositionAsync({ accuracy: Location.Accuracy.Balanced, timeInterval: 100, distanceInterval: 1 },
-        position => {
-          setLocation(position.coords);
-        })
+	//////////데이터 받기///////////////
+	// axios 사용 버전
+	// useEffect(() => {
+	// 	if (location) {
+	// 		const { latitude, longitude } = location;
+	// 		axios
+	// 			.get("http://localhost:8080/map/location", {
+	// 				params: {
+	// 					latitude,
+	// 					longitude,
+	// 				},
+	// 			})
+	// 			.then((response) => {
+	// 				console.log(latitude, longitude);
+	// 				console.log("suc");
+	// 				setMarkers(response.data);
+	// 			})
+	// 			.catch((error) => {
+	// 				console.log(error);
+	// 			});
+	// 	}
+	// }, [location]);
 
+	//fetch 사용버전
+	// useEffect(() => {
+	// 	if (location) {
+	// 		const { latitude, longitude } = location;
+	// 		fetch(
+	// 			`http://apis.data.go.kr/B552657/ErmctInfoInqireService/getEgytListInfoInqire?serviceKey=NrQI5b0K8twIELQnktYwdAABNNMSCg3uPfykA6hogdCNwbKlk7huaHMXzbWJ%2F0nFzsQTKGIqQbIBcUG21fgZ7A%3D%3D&Q0=경기도용인시처인&pageNo=1&numOfRows=30&yPos=${latitude}&xPos=${longitude}`
+	// 		)
+	// 			.then((response) => response.json())
+	// 			.then((data) => {
+	// 				setMarkers(data);
+	// 			})
+	// 			.catch((error) => {
+	// 				console.log(error);
+	// 			});
+	// 	}
+	// }, [location]);
 
-    })();
-  }, []);
+	//dummy data 사용버전
+	// useEffect(() => {
+	// 	setMarkers(markersData);
+	// }, []);
 
-  let text = 'Waiting..'
-  let mylat = '';
-  let mylong = '';
-  if (errorMsg) {
-    text = errorMsg;
-  } else if (location) {
-    text = JSON.stringify(location);
-    mylat = location.latitude;
-    mylong = location.longitude;
-  }
-  // ?lat=${mylat}&lng=${mylong}
+	// const handleMarkerPress = (markerId, markerTitle) => {
+	// 	setSelectedMarker({ id: markerId, title: markerTitle });
+	// };
 
-  // axios.get(`https://6cbfa40d-1b54-4b5d-8c72-774f14fba01d.mock.pstmn.io`,
-  axios.get(`http://localhost:8080/map/location`,
-    {
-      params: {
-        mylat,
-        mylong,
-      }
-    })
-    .then((response) => {
-      console.log(mylat, mylong);
-      console.log("succ")
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-  // fetch('https://dummyjson.com/products/1')
-  //   .then(res => res.json())
-  //   .then(json => console.log(json))
+	//Marker 설명 하단에 적을 때 사용
+	const showMarkerInfo = (marker) => {
+		const defaultInfo = {
+			title: "마커 정보 없음",
+			description: "설명 없음",
+		};
 
+		const displayedInfo = {
+			title: marker.title || defaultInfo.title,
+			description: marker.description || defaultInfo.description,
+		};
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.paragraph}>{mylat}</Text>
-      <Text style={styles.paragraph}>{mylong}</Text>
-      <MapView style={styles.map}
-        region={{
-          latitude: mylat,
-          longitude: mylong,
-          latitudeDelta: 0.00999,
-          longitudeDelta: 0.00521,
-        }}
-        provider={PROVIDER_GOOGLE}
-      >
-        <Marker coordinate={{
-          // latitude: location && mylat,
-          // longitude: location && mylong
-          latitude: location ? mylat : 0,
-          longitude: location ? mylong : 0
-        }}
-          title="현재 위치"
-          description="주소" />
-      </MapView>
-    </View>
-  );
+		setMarkerInfo(displayedInfo);
+	};
+
+	let mylat = location?.latitude || 0;
+	let mylong = location?.longitude || 0;
+
+	return (
+		<View style={styles.container}>
+			<Text style={styles.paragraph}>{mylat}</Text>
+			<Text style={styles.paragraph}>{mylong}</Text>
+			<MapView
+				style={styles.map}
+				region={{
+					latitude: mylat,
+					longitude: mylong,
+					latitudeDelta: 0.00999,
+					longitudeDelta: 0.00521,
+				}}
+				provider={PROVIDER_GOOGLE}
+			>
+				<Marker
+					coordinate={{
+						latitude: mylat,
+						longitude: mylong,
+					}}
+					onPress={() => {
+						console.log("Pressed");
+					}}
+					title="현재 위치"
+					description="주소"
+				/>
+
+				{/* Marker 여러개 생성 시 사용 */}
+				{markers.map((marker, index) => (
+					<Marker
+						key={index}
+						coordinate={{
+							latitude: marker.latitude,
+							longitude: marker.longitude,
+						}}
+						title={marker.title}
+						description={marker.description}
+					>
+						<Callout>
+							<TouchableOpacity
+								onPress={() => showMarkerInfo(marker)}
+							>
+								<View>
+									<Text>{marker.title}</Text>
+									<Text>{marker.description}</Text>
+								</View>
+							</TouchableOpacity>
+						</Callout>
+					</Marker>
+				))}
+				{/* marker test */}
+				<Marker
+					coordinate={{
+						latitude: 37.541895,
+						longitude: 127.013,
+					}}
+					title="hospital"
+					description="seoul hospital"
+					onPress={() => console.log("pressed")}
+				/>
+			</MapView>
+			{markerInfo && (
+				<View style={styles.markerInfo}>
+					<Text>Title: {markerInfo.title}</Text>
+					<Text>Description: {markerInfo.description}</Text>
+				</View>
+			)}
+
+			<Text>Minjoon</Text>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  paragraph: {
-    fontSize: 18,
-    textAlign: 'center',
-  },
-  map: {
-    width: "80%",
-    height: "80%",
-  }
+	container: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+		padding: 20,
+	},
+	paragraph: {
+		fontSize: 18,
+		textAlign: "center",
+	},
+	map: {
+		width: Dimensions.get("window").height,
+		height: "80%",
+	},
 });
---
+++ markerData.js
@@ -0,0 +1,46 @@
+const markersData = [
+    {
+        id: 1,
+        latitude: 37.550702,
+        longitude: 126.924228,
+        title: "Hospital 1",
+        description: "Seoul Hospital 1",
+    },
+    {
+        id: 2,
+        latitude: 37.540702,
+        longitude: 126.914228,
+        title: "Hospital 2",
+        description: "Seoul Hospital 2",
+    },
+    {
+        id: 3,
+        latitude: 37.5407,
+        longitude: 126.9142,
+        title: "Hospital 3",
+        description: "Seoul Hospital 3",
+    },
+    {
+        id: 4,
+        latitude: 37.540706,
+        longitude: 126.914223,
+        title: "Hospital 4",
+        description: "Seoul Hospital 4",
+    },
+    {
+        "id": 5,
+        "latitude": 37.538788,
+        "longitude": 127.03377,
+        "title": "서울랜드",
+        "description": "서울특별시 서울랜드"
+    },
+    {
+        "id": 6,
+        "latitude": 37.517364,
+        "longitude": 127.047224,
+        "title": "잠실종합운동장",
+        "description": "서울특별시 잠실 종합운동장"
+    },
+];
+
+export default markersData;
